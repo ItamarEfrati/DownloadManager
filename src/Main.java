c09151a7522dbc1dad53a1b2326de2c1fa24dd75
Yarden <yardenrachamim1993@gmail.com>
2020-01-04 20:05:45 +0200

    First!

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void Main(String[] args){
+        // TODO: read the arguments and call for download manager
+    }
+}
---
+++ src/PacketDownloader.java
@@ -0,0 +1,2 @@
+public class PacketDownloader {
+}
